e6c16218133afd9e011f1a2bfd360a98ad2d2f18
Om4r37 <32101001031@std.bau.edu.jo>
2024-08-12 00:34:38 +0300

    fix minor stuff

--- schema.sql
+++ schema.sql
@@ -13,7 +13,6 @@ CREATE TABLE users (
     residence boolean, -- 0: rural, 1: urban
     smoke INTEGER, -- 0: unknown, 1: never, 2: former, 3: current
     pregnancies INTEGER,
-    -- cp INTEGER, -- 1: typical angina, 2: atypical angina, 3: non-anginal pain, 4: asymptomatic
     exng boolean,
     heart_disease boolean
 );
