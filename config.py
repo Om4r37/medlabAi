d2999d99d708cff9224ebcd5cdb0c8e810ce16be
Om4r37 <32101001031@std.bau.edu.jo>
2024-08-11 15:23:05 +0300

    clarify configs

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ LOCATIONS = ("main lab", "secondary lab")
 TEST_TYPES = {"diabetes": 10, "stroke": 20, "heart attack": 30, "heart failure": 30}
 
 
+# flask config
 class Config:
     SESSION_PERMANENT = False
     SESSION_TYPE = "filesystem"
