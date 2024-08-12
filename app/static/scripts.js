fa4beed7599b9b4e4c46cc4d0d486b8a81c1f327
Om4r37 <32101001031@std.bau.edu.jo>
2024-08-12 17:54:01 +0300

    sort by time

--- app/static/scripts.js
+++
@@ -1,9 +0,0 @@
-function closeFlash() {
-    document.getElementById('flash').style.display = 'none';
-    document.getElementsByTagName('main')[0].style.marginTop = '15px';
-}
-function highlight(childNo) {
-    const navbarTabs = document.querySelectorAll('.navbar-tab');
-    navbarTabs[childNo].classList.add('highlight');
-    navbarTabs[childNo + 3].classList.add('highlight');
-}
