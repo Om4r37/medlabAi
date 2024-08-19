8c1375d14b6f3024479b9118187e46d84b9e0f30
Om4r37 <32101001031@std.bau.edu.jo>
2024-08-19 08:29:43 +0300

    add gender, residence, exng and heart disease donut charts

--- faker.py
+++ faker.py
@@ -5,4 +5,6 @@ from random import randint
 from config import STATS
 
 for stat in STATS:
-    db.execute("UPDATE stats SET value = ? WHERE name = ?;", randint(0, 1000), stat)
+    db.execute("UPDATE stats SET value = ? WHERE name = ?;", randint(0, 800), stat)
+
+db.execute("UPDATE stats SET value = 990 WHERE name = 'users_count';")
