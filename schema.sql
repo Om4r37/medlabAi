fedb1c2ab061df8d0f7268cf2b0c0e9d4076a6ca
OMAR ALNADI <129601065+Om4r37@users.noreply.github.com>
2024-08-25 11:59:47 +0300

    add users page (#50)

--- schema.sql
+++ schema.sql
@@ -9,7 +9,7 @@ CREATE TABLE users (
     height INTEGER,
     weight INTEGER,
     married boolean,
-    work INTEGER, -- stroke (never worked, private, self-employed, gov, children)
+    work INTEGER, -- (never worked, private, self-employed, gov, children)
     residence boolean, -- 0: rural, 1: urban
     smoke INTEGER, -- 0: unknown, 1: never, 2: former, 3: current
     pregnancies INTEGER,
