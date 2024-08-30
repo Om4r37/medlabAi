490eecb0b65deb19a5ab66afbf2f155b915afbf9
Om4r37 <32101001031@std.bau.edu.jo>
2024-08-30 10:12:38 +0300

    add /change_password

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -42,6 +42,32 @@ def register():
     return redirect("/")
 
 
+@bp.route("/change_password", methods=["GET", "POST"])
+def change_password():
+    """Change user password"""
+    if request.method == "GET":
+        return render_template("auth/change_password.jinja")
+
+    password = request.form.get("password")
+    verification = request.form.get("confirmation")
+
+    if not password:
+        return render_template(
+            "error.jinja", message="must provide new password", code=400
+        )
+
+    if password != verification:
+        return render_template("error.jinja", message="passwords don't match", code=400)
+
+    db.execute(
+        "UPDATE users SET hash = ? WHERE id = ?;",
+        generate_password_hash(password),
+        session["user_id"],
+    )
+    flash("Password Changed Successfully!")
+    return redirect("/")
+
+
 @bp.route("/login", methods=["GET", "POST"])
 def login():
     """Log user in"""
