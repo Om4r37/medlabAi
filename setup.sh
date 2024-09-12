7a5c6239d6041e6b1e78513e350ad9522d1586ab
OMAR ALNADI <129601065+Om4r37@users.noreply.github.com>
2024-09-12 16:44:14 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,9 +5,7 @@ wget https://github.com/Om4r37/medlabAi/releases/download/v0.9/data.zip
 unzip data.zip
 rm -f data.zip
 cd ..
-pip install -r requirements.txt
 wget https://github.com/Om4r37/medlabAi/releases/download/v0.9/users.sql
-cd medlabAi/
 python -m venv .venv
 source .venv/bin/activate
 pip install -r requirements.txt
